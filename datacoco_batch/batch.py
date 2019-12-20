1caf712776992bff38ecebeb1351d2d8e33b9fa3
Gianna Miggins <Gianna.Miggins@equinox.com>
2019-12-20 16:03:52 -0500

    black formatting

--- datacoco_batch/batch.py
+++ datacoco_batch/batch.py
@@ -5,13 +5,14 @@ class Batch:
     """
     integrated integration to Batchy batch control system
     """
-    def __init__(self, wf: str, server='0.0.0.0', port='8050'):
+
+    def __init__(self, wf: str, server="0.0.0.0", port="8050"):
         self.wf = wf
         # different URL if behind load balancer
-        if "hypergate" in server and port == '80':
-            self.server_url = 'http://' + server + '/batchy'
+        if "hypergate" in server and port == "80":
+            self.server_url = "http://" + server + "/batchy"
         else:
-            self.server_url = 'http://' + server + ':' + port
+            self.server_url = "http://" + server + ":" + port
 
     def open(self, fmt=None):
         """
@@ -22,13 +23,14 @@ class Batch:
         :return:
         """
         r = None
-        if fmt == 'json':
-            r = requests.get(self.server_url + '/open_batch/' + self.wf).text()
-        elif fmt == 'infa':
-            r = requests.get(self.server_url + '/open_batch/infa/' +
-                             self.wf).text()
+        if fmt == "json":
+            r = requests.get(self.server_url + "/open_batch/" + self.wf).text()
+        elif fmt == "infa":
+            r = requests.get(
+                self.server_url + "/open_batch/infa/" + self.wf
+            ).text()
         else:
-            r = requests.get(self.server_url + '/open_batch/' + self.wf).json()
+            r = requests.get(self.server_url + "/open_batch/" + self.wf).json()
         return r
 
     def close(self):
@@ -37,7 +39,7 @@ class Batch:
         :param wf:
         :return:
         """
-        r = requests.get(self.server_url + '/close_batch/' + self.wf).json()
+        r = requests.get(self.server_url + "/close_batch/" + self.wf).json()
         return r
 
     def fail(self):
@@ -46,7 +48,7 @@ class Batch:
         :param wf:
         :return:
         """
-        r = requests.get(self.server_url + '/fail_batch/' + self.wf).json()
+        r = requests.get(self.server_url + "/fail_batch/" + self.wf).json()
         return r
 
     def get_status(self):
@@ -55,7 +57,7 @@ class Batch:
         :param wf:
         :return:
         """
-        r = requests.get(self.server_url + '/get_status/' + self.wf).json()
+        r = requests.get(self.server_url + "/get_status/" + self.wf).json()
         return r
 
     def get_status_summary(self):
@@ -64,19 +66,34 @@ class Batch:
         :return:
         """
         details = self.get_status()
-        r = {'batch_id': 0, 'status': 'success', 'failure_cnt': 0,
-             'open_cnt': 0, 'batch_start': '', 'batch_end': ''}
+        r = {
+            "batch_id": 0,
+            "status": "success",
+            "failure_cnt": 0,
+            "open_cnt": 0,
+            "batch_start": "",
+            "batch_end": "",
+        }
 
         for x in details.items():
-            if x.get('status') == 'failure':
-                r['failure_cnt'] += 1
-            if x.get('status') == 'open':
-                r['open_cnt'] += 1
-            r['batch_start'] = x.get('batch_start') if r['batch_start'] is None or x.get('batch_start') < r['batch_start'] else r['batch_start']
-            r['batch_end'] = x.get('batch_end') if 'batch_end' in r or x.get('batch_end') > r['batch_end'] else r['batch_end']
-            r['batch_id'] = x.get('batch_id')
-        if r['failure_cnt'] > 0:
-            r['status'] = 'failure'
-        elif r['open_cnt'] > 0:
-            r['status'] = 'open'
+            if x.get("status") == "failure":
+                r["failure_cnt"] += 1
+            if x.get("status") == "open":
+                r["open_cnt"] += 1
+            r["batch_start"] = (
+                x.get("batch_start")
+                if r["batch_start"] is None
+                or x.get("batch_start") < r["batch_start"]
+                else r["batch_start"]
+            )
+            r["batch_end"] = (
+                x.get("batch_end")
+                if "batch_end" in r or x.get("batch_end") > r["batch_end"]
+                else r["batch_end"]
+            )
+            r["batch_id"] = x.get("batch_id")
+        if r["failure_cnt"] > 0:
+            r["status"] = "failure"
+        elif r["open_cnt"] > 0:
+            r["status"] = "open"
         return r
